c9edbaa8cbcfd8e2ea010945543511101edccfe4
sainath1431 <wankhedesainath1431@gmail.com>
2024-07-19 11:11:04 +0530

    added Day2

---
+++ Day-2/controlStatement.py
@@ -0,0 +1,11 @@
+#check the weekend tour place using the elif ladder 
+place = input("Enter Your Place : ");
+
+if place == "Goa":
+    print("Challo Goa");
+elif place == "Manali":
+    print("Challo Manali");
+elif place == "Kerala":
+    print("Challo kerala");
+else:
+    print("In valid place");
